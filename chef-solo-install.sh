43f95c25e0b78df0a7ad6bb48a68f3521cdd625c
CarettaCaretta <luca1.pandini@gmail.com>
2012-12-06 18:51:48 +0100

    Delete chef-solo-install.sh

--- chef-solo-install.sh
+++
@@ -1,8 +0,0 @@
-#echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
-sudo mkdir -p /etc/apt/trusted.gpg.d
-gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
-gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
-sudo apt-get update
-sudo apt-get install opscode-keyring # permanent upgradeable keyring
-sudo apt-get upgrade
-echo "chef chef/chef_server_url string none" | sudo debconf-set-selections && sudo apt-get install chef -y
